56338c10101328a138c5579eedea9a21a47e0c90
Max Schwarzer <maxon742@gmail.com>
2016-04-21 21:31:43 -0700

    Setting CRAN Mirror per an R error message. See #1

--- parking_analysis.R
+++ parking_analysis.R
@@ -21,7 +21,7 @@ install_libraries <- function(){
   # }
   list.of.packages <- c("ggplot2", "plyr", "lubridate")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) install.packages(new.packages)
+  if(length(new.packages)) install.packages(new.packages, dependencies = TRUE, repos='http://cran.us.r-project.org') 
   
   library(ggplot2)
   library(plyr)
